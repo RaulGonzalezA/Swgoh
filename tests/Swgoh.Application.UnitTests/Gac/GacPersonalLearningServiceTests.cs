eaa989369ec32f2801f3f3894576d8e2bad0fcf0
Raul Gonzalez <raulgonzalezag@hotmail.com>
2026-09-13 20:39:38 +0200

    Add personal learning service test shell

---
+++ tests/Swgoh.Application.UnitTests/Gac/GacPersonalLearningServiceTests.cs
@@ -0,0 +1,10 @@
+using Swgoh.Application.Abstractions;
+using Swgoh.Application.Gac;
+using Swgoh.Domain.Gac;
+using Xunit;
+
+namespace Swgoh.Application.UnitTests.Gac;
+
+public sealed class GacPersonalLearningServiceTests
+{
+}
