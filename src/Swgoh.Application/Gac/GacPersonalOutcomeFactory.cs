7d57444e46c61fa7aa62577f2138b70836fb32f3
Raul Gonzalez <raulgonzalezag@hotmail.com>
2026-09-13 20:29:33 +0200

    Build personal outcomes from planner state

--- src/Swgoh.Application/Gac/GacPersonalOutcomeFactory.cs
+++ src/Swgoh.Application/Gac/GacPersonalOutcomeFactory.cs
@@ -4,6 +4,37 @@ namespace Swgoh.Application.Gac;
 
 internal static class GacPersonalOutcomeFactory
 {
+    public static GacPersonalRoundOutcome FromState(GacPlannerState state)
+    {
+        ArgumentNullException.ThrowIfNull(state);
+
+        Dictionary<Guid, GacVisibleDefenseDetails> defenses = state.Plan.VisibleDefenses.ToDictionary(item => item.Id);
+        GacPersonalAttackOutcome[] outcomes =
+        [
+            .. state.Plan.Attacks
+                .Where(attack => attack.Status is GacAttackPlanStatus.Won or GacAttackPlanStatus.Failed)
+                .Where(attack => defenses.ContainsKey(attack.DefenseId))
+                .OrderBy(attack => attack.DefenseId)
+                .ThenBy(attack => attack.Attempt)
+                .Select(attack => new GacPersonalAttackOutcome(
+                    attack.Id,
+                    attack.Attempt,
+                    attack.Status,
+                    ToDomain(defenses[attack.DefenseId].Squad, state.Plan.Format),
+                    ToDomain(attack.Team.Squad, state.Plan.Format)))
+        ];
+
+        return new GacPersonalRoundOutcome(
+            state.Plan.Id,
+            state.Plan.PlayerAllyCode,
+            state.Plan.OpponentAllyCode,
+            state.Plan.EventInstanceId,
+            state.Plan.RoundNumber,
+            state.Plan.Format,
+            outcomes,
+            state.Plan.UpdatedAtUtc);
+    }
+
     public static GacPersonalRoundOutcome FromPlan(
         GacRoundPlan plan,
         IReadOnlyDictionary<Guid, GacTeamPreset> presets)
@@ -37,4 +68,11 @@ internal static class GacPersonalOutcomeFactory
             outcomes,
             plan.UpdatedAtUtc);
     }
+
+    private static GacPlannerSquad ToDomain(GacPlannerSquadDetails squad, GacFormat format) =>
+        GacPlannerSquad.Create(
+            format,
+            squad.Leader.DefinitionId,
+            squad.Members.Select(unit => unit.DefinitionId),
+            squad.IsFleet);
 }
