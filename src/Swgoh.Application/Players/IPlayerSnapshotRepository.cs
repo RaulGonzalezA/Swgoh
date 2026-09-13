6803e658138c90f189d079f8fa3984883b99a58b
Raul Gonzalez <raulgonzalezag@hotmail.com>
2026-09-13 10:48:10 +0200

    Add snapshot existence check

--- src/Swgoh.Application/Players/IPlayerSnapshotRepository.cs
+++ src/Swgoh.Application/Players/IPlayerSnapshotRepository.cs
@@ -2,6 +2,8 @@ namespace Swgoh.Application.Players;
 
 public interface IPlayerSnapshotRepository
 {
+    Task<bool> ExistsAsync(string id, CancellationToken cancellationToken = default);
+
     Task UpsertAsync(PlayerSnapshot snapshot, CancellationToken cancellationToken = default);
 
     Task<IReadOnlyCollection<PlayerSnapshot>> GetRecentAsync(long allyCode, int limit, CancellationToken cancellationToken = default);
