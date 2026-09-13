621fda59f834be31a5590bddd39b428ccfd47ee5
Raul Gonzalez <raulgonzalezag@hotmail.com>
2026-09-13 20:29:04 +0200

    Register personal GAC learning service

--- src/Swgoh.Application/DependencyInjection.cs
+++ src/Swgoh.Application/DependencyInjection.cs
@@ -24,6 +24,7 @@ public static class DependencyInjection
         services.AddScoped<IGacCounterStatisticsService, GacCounterStatisticsService>();
         services.AddScoped<IOpponentScoutingService, OpponentScoutingService>();
         services.AddScoped<ICurrentGacScoutingService, CurrentGacScoutingService>();
+        services.AddScoped<IGacPersonalLearningService, GacPersonalLearningService>();
         services.AddScoped<IGacPlannerService, GacPlannerService>();
         services.AddScoped<IGacAttackPlanOptimizerService, GacAttackPlanOptimizerService>();
         return services;
