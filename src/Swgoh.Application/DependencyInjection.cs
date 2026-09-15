a7e383ee095e793e21b328e8df53f289f1c7b4c7
Raul Gonzalez <raulgonzalezag@hotmail.com>
2026-09-15 15:03:42 +0200

    feat: register roster defense candidate service

--- src/Swgoh.Application/DependencyInjection.cs
+++ src/Swgoh.Application/DependencyInjection.cs
@@ -40,6 +40,7 @@ public static class DependencyInjection
         services.AddScoped<IGacPlannerMutationService, GacPlannerMutationService>();
         services.AddScoped<GacDefenseStrategyService>();
         services.AddScoped<IGacDefenseStrategyService, GacDefenseStrategyService>();
+        services.AddScoped<GacRosterDefenseCandidateService>();
         services.AddScoped<IGacSmartDefenseService, GacSmartDefenseService>();
         services.AddScoped<GacAttackPlanOptimizerService>();
         services.AddScoped<IGacAttackPlanOptimizerService, HardenedGacAttackPlanOptimizerService>();
