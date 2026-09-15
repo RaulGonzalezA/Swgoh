87277b18ade03ad97e55aa607e3ccd901d8eb6f2
Raul Gonzalez <raulgonzalezag@hotmail.com>
2026-09-15 10:45:36 +0200

    Register GAC planner performance client

--- src/Swgoh.Blazor/Program.cs
+++ src/Swgoh.Blazor/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddHttpClient<PlayerApiClient>(client => client.BaseAddress = n
 builder.Services.AddHttpClient<GacApiClient>(client => client.BaseAddress = new Uri("https+http://api"));
 builder.Services.AddHttpClient<GacScoutingCacheApiClient>(client => client.BaseAddress = new Uri("https+http://api"));
 builder.Services.AddHttpClient<GacPlannerApiClient>(client => client.BaseAddress = new Uri("https+http://api"));
+builder.Services.AddHttpClient<GacPlannerPerformanceApiClient>(client => client.BaseAddress = new Uri("https+http://api"));
 builder.Services.AddHttpClient<GacAttackExecutionApiClient>(client => client.BaseAddress = new Uri("https+http://api"));
 builder.Services.AddHttpClient<ConquestApiClient>(client => client.BaseAddress = new Uri("https+http://api"));
 builder.Services.AddHttpClient<ConquestDailyPlanApiClient>(client => client.BaseAddress = new Uri("https+http://api"));
