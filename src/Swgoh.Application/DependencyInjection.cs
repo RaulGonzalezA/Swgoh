b8b134a621ada19dbd5e41e858c209298b692701
Raul Gonzalez <raulgonzalezag@hotmail.com>
2026-09-16 12:47:33 +0200

    feat: register RotE mission guide service

--- src/Swgoh.Application/DependencyInjection.cs
+++ src/Swgoh.Application/DependencyInjection.cs
@@ -24,6 +24,7 @@ public static class DependencyInjection
         services.AddScoped<IConquestService, ConquestService>();
         services.AddScoped<IConquestDailyPlanService, ConquestDailyPlanService>();
         services.AddScoped<IRiseOfEmpireService, RiseOfEmpireService>();
+        services.AddScoped<IRiseOfEmpireMissionGuideService, RiseOfEmpireMissionGuideService>();
         services.AddScoped<IRiseOfEmpireGuildService, RiseOfEmpireGuildService>();
         services.AddSingleton<IGacRulesService, GacRulesService>();
         services.AddSingleton<IGacTelemetry, GacTelemetry>();
